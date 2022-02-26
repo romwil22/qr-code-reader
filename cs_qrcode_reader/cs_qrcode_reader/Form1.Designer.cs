92d690605bcf56d081aa60f6b4ac41e4ea94b8e0
Romwil <romwil.pilapil@gmail.com>
2022-02-27 04:06:12 +0800

    uploading qr code program

---
+++ cs_qrcode_reader/cs_qrcode_reader/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace cs_qrcode_reader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.camera_device_combox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.qrcode_picturebox = new System.Windows.Forms.PictureBox();
+            this.qrcode_textbox = new System.Windows.Forms.TextBox();
+            this.start_button = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.qrcode_picturebox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // camera_device_combox
+            // 
+            this.camera_device_combox.FormattingEnabled = true;
+            this.camera_device_combox.Location = new System.Drawing.Point(81, 9);
+            this.camera_device_combox.Name = "camera_device_combox";
+            this.camera_device_combox.Size = new System.Drawing.Size(269, 21);
+            this.camera_device_combox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Camera:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(490, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "QR Code Text";
+            // 
+            // qrcode_picturebox
+            // 
+            this.qrcode_picturebox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.qrcode_picturebox.Location = new System.Drawing.Point(15, 36);
+            this.qrcode_picturebox.Name = "qrcode_picturebox";
+            this.qrcode_picturebox.Size = new System.Drawing.Size(469, 402);
+            this.qrcode_picturebox.TabIndex = 2;
+            this.qrcode_picturebox.TabStop = false;
+            // 
+            // qrcode_textbox
+            // 
+            this.qrcode_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.qrcode_textbox.Location = new System.Drawing.Point(493, 56);
+            this.qrcode_textbox.Multiline = true;
+            this.qrcode_textbox.Name = "qrcode_textbox";
+            this.qrcode_textbox.Size = new System.Drawing.Size(295, 303);
+            this.qrcode_textbox.TabIndex = 3;
+            // 
+            // start_button
+            // 
+            this.start_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start_button.Location = new System.Drawing.Point(507, 381);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(75, 37);
+            this.start_button.TabIndex = 4;
+            this.start_button.Text = "Start";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(588, 381);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Read";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.start_button);
+            this.Controls.Add(this.qrcode_textbox);
+            this.Controls.Add(this.qrcode_picturebox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.camera_device_combox);
+            this.Name = "Form1";
+            this.Text = "QR Code Reader";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.qrcode_picturebox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox camera_device_combox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox qrcode_picturebox;
+        private System.Windows.Forms.TextBox qrcode_textbox;
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ cs_qrcode_reader/cs_qrcode_reader/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using AForge;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+using ZXing.Aztec;
+
+namespace cs_qrcode_reader
+{
+    public partial class Form1 : Form
+    {
+        public FilterInfoCollection captureDevice;
+        public VideoCaptureDevice finalFrame;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            captureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in captureDevice)
+            {
+                camera_device_combox.Items.Add(device.Name);
+            }
+
+            camera_device_combox.SelectedIndex = 0;
+            finalFrame = new VideoCaptureDevice();
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            finalFrame = new VideoCaptureDevice(captureDevice[camera_device_combox.SelectedIndex].MonikerString);
+            finalFrame.NewFrame += new NewFrameEventHandler(finalframe_newframe);
+            finalFrame.Start();
+        }
+
+        private void finalframe_newframe(object sender, NewFrameEventArgs eventArgs)
+        {
+            qrcode_picturebox.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (finalFrame.IsRunning == true)
+            {
+                finalFrame.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            BarcodeReader qrCodeReader = new BarcodeReader();
+            Result qrResult = qrCodeReader.Decode((Bitmap)qrcode_picturebox.Image);
+
+            try
+            {
+                string qrDecoded = qrResult.ToString().Trim();
+                if (qrDecoded != "")
+                {
+                    qrcode_textbox.Text = qrDecoded;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
